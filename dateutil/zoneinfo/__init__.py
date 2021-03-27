3cae95c859d1827d114b387125f60d6b55028b51
Fangchen Li <fangchen.li@outlook.com>
2021-03-26 22:09:42 -0500

    more compat

--- dateutil/zoneinfo/__init__.py
+++ dateutil/zoneinfo/__init__.py
@@ -21,7 +21,7 @@ class tzfile(_tzfile):
 def getzoneinfofile_stream():
     try:
         return BytesIO(get_data(__name__, ZONEFILENAME))
-    except OSError as e:  # TODO  switch to FileNotFoundError?
+    except FileNotFoundError as e:
         warnings.warn(f"I/O error({e.errno}): {e.strerror}")
         return None
 
